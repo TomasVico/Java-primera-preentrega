609f9fe6b37c20a0d81249c246385d1593fe8b9c
Tomas Vico <tvico97@hotmail.com>
2023-10-31 13:25:03 -0300

    Primera preentrega finalizada

--- 1era preentrega/main.js
+++ 1era preentrega/main.js
@@ -1,6 +1,9 @@
-
-
-function darNota(){
+/*
+Que el usuario ingrese el costo del producto que quiere comprar
+Luego que ingrese la cantidad de cuotas en las que desea pagarlo
+y el sistema le indique el porcentaje de interés total,el precio final y el precio de cada cuota
+*/
+/*function darNota(){
     let nombre= prompt("Ingresá tu nombre: ")
     while(nombre!="salir"){    
         
@@ -12,17 +15,50 @@ function darNota(){
             else{
                 alert("Lo lamento "+nombre+ ", no aprobaste")
                 
-            }
-        nombre=prompt("Ingrese nombre de otro alumno: ")
-        
-        }
-        
-    
+        }        
     }
+}
+*/
 
+
+while(true){
+    let nombre=prompt("Ingrese su nombre para una atencion personalizada, o 'salir' para terminar: ")
+    if(nombre=="salir"){
+        console.log("Gracias por su visita!.")
+        break
+    }
+    var precio= prompt("Ingrese el precio del producto que le interesa comprar: ") 
+    let cuotas=prompt("Ingrese en la cantidad de cuotas que desea comprarlo: ")
+    let preciocuotas= precio/cuotas
+    if (cuotas>=1 && cuotas <=24){ 
+        if (cuotas<=6){
+            var precio= precio
+            console.log("Hola "+nombre+"! el precio de su producto será $"+precio+" y lo pagarás en "+cuotas+" cuotas de $"+ preciocuotas+" cada una")
+        }else if(cuotas >6 && cuotas <=12){
+            var precio= precio * 1.3
+            console.log("Hola "+nombre+"! el precio de su producto será $"+precio+" y lo pagarás en "+cuotas+" cuotas de $"+ preciocuotas+" cada una")
+        }else{
+            var precio = precio *2
+            console.log("Hola "+nombre+"! el precio de su producto será $"+precio+" y lo pagarás en "+cuotas+" cuotas de $"+ preciocuotas+" cada una")
+        }
     
 
+        
+    }else{
+        console.log("Debes elegir de 1 a 24 cuotas")
+        break
+    }
+}
 
-darNota()
+    
+    
+    
 
-
+    
+/* if(cuotas <=6){
+    preciocuotas=precio /cuotas;
+}else if(cuotas >6 && cuotas <=12){
+    preciocuotas= precio/ cuotas; 
+}else (cuotas >12 && cuotas <=24);{
+    preciocuotas= precio / cuotas;
+    */
